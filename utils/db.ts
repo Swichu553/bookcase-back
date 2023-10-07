7acc55aae6ea4e401904d40353039ddb10ca425d
Swichu553 <swicha@o2.pl>
2023-10-07 19:31:40 +0200

    add db config

---
+++ utils/db.ts
@@ -0,0 +1,12 @@
+import { createPool } from "mysql2/promise";
+import { config } from "../config/config";
+
+const { dbHost, dbUser, dbDatabase } = config;
+
+export const pool = createPool({
+    host: dbHost,
+    user: dbUser,
+    database: dbDatabase,
+    namedPlaceholders: true,
+    decimalNumbers: true,
+})
