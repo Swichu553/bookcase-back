3ed5f4de9171cafbffcc7600fc4241498435eb5a
Swichu553 <swicha@o2.pl>
2023-10-07 19:39:58 +0200

    add json, cors and rateLimit

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
+import { config } from './config/config';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
-
-
+app.use(cors({
+    origin: config.corsOrigin,
+}));
+app.use(json());
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, //15min
+    max: 100, //limit
+}));
 
 
 app.listen(3001, '0.0.0.0', () => {
