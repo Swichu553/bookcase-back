a35a2b8d2ee96b3b5de06854c777363c23b8b975
Swichu553 <swicha@o2.pl>
2023-10-10 18:12:43 +0200

    add typees adBook

---
+++ types/adBook/adBook-entity.ts
@@ -0,0 +1,10 @@
+export interface AdBookEntity {
+    isbn: string;
+    title: string;
+    author: string;
+    publisher: string;
+    publisherDate: Date;
+    categories: string;
+    rating: string;
+    description: string;
+}
