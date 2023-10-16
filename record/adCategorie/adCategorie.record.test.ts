1a05bd6d6796ac0e5c01a072947fe26e6c872639
Swichu553 <swicha@o2.pl>
2023-10-16 19:44:43 +0200

    test adCategories

--- record/adCategorie/adCategorie.record.test.ts
+++ record/adCategorie/adCategorie.record.test.ts
@@ -8,6 +8,12 @@ const defCat = {
 test("Sprawdzenie dodawania kategori", async () => {
     const cat = new AdCategoryRecord(defCat);
     expect(cat.name).toBe("Komedia");
-    await cat.insertCategorie();
 });
 
+test('Pobieranie kategori', async () => {
+    const testCat = await AdCategoryRecord.getOneCategorie('dbf092b2-2abe-4e4c-a8fd-371c58e658cd');
+
+    expect(testCat.name).toBe('Komedia');
+    expect(testCat.description).toBe('Smieszne książki');
+
+});
